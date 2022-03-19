ae75eaa60877d0e9fdfbe1c8346fd0d6bf34b770
kritical0613 <kritical0613@outlook.com>
2022-03-19 01:05:45 -0500

    fix: solutions md responsive

--- src/components/IllustComponent/index.tsx
+++ src/components/IllustComponent/index.tsx
@@ -9,8 +9,8 @@ interface IllustComponentProps {
 
 const IllustComponent: React.FC<IllustComponentProps> = ({ illust, title = "", content, isRTL = false }) => {
   return (
-    <div className="grid md:flex gap-x-24 m-auto justify-items-center items-center">
-      <div className={`flex ${isRTL ? "md:order-last" : "md:order-first"} mt-10 md:mt-0 order-last w-3/4 md:w-auto min-w-[30%] justify-center`}><img src={illust} alt="Illustrs" /></div>
+    <div className="grid lg:flex gap-x-24 md:gap-y-12 gap-y-0 m-auto justify-items-center items-center">
+      <div className={`flex ${isRTL ? "lg:order-last" : "lg:order-first"} mt-10 md:mt-0 order-last w-3/4 md:w-auto min-w-[30%] justify-center`}><img src={illust} alt="Illustrs" /></div>
       <div className="flex-row space-y-4">
         <div className="font-bold uppercase text-[19px] md:text-[28px] leading-[23px] md:leading-[32.81px] tracking-wider max-w-xl">{title}</div>
         <div className="font-thin text-[17px] md:text-[20px] tracking-wide px-2 indent-3 leading-[21px] md:leading-[28.13px] max-w-xl">{content}</div>
